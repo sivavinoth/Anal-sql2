2c927ea729c7afdad496897009d9aee9bc276752
sivavinothjm <mjsivavinoth@gmail.com>
2023-03-30 16:00:30 +0530

    created table

---
+++ insert_data.sql
@@ -0,0 +1,5 @@
+INSERT INTO users(names,salary,current_status) VALUES('surya',180000,'self-employed');
+INSERT INTO users VALUES('siva vinoth',190000,'self-employed');
+INSERT INTO users VALUES('richard',150000,'employed');
+INSERT INTO users VALUES('riyas',110000,'unemployed');
+INSERT INTO users VALUES('micheal',130000,'unemployed');
